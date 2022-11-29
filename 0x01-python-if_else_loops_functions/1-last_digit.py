a63b63070fff7d2d209c02c04accbce8f001d99b
Vicsituma <vicsituma45@gmail.com>
2022-11-29 20:29:59 +0000

    alphabets

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,4 +10,4 @@ elif digit == 0:
     print(f"Last digit of {number:d} is {digit:d} and is 0")
 else:
     print(f"Last digit of {number:d} is {digit:d} and is less\
-    than 6 and not 0")
+ than 6 and not 0")
