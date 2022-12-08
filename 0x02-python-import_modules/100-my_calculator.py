3d7e24180af8a90cc41903dddfb4b0ce1e3723be
Vicsituma <vicsituma45@gmail.com>
2022-12-08 03:09:36 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1.py import *
-    a = int(argv[1])
-    b = int(argv[3])
-    if len(argv) != 4:
+    args = sys.argv[1:]
+    a = int(args[0])
+    b = int(args[2])
+    if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    if argv[2] not in ["+", "-", "*", "/"]:
+        sys.exit(1)
+    if arg1[2] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    if argv[2] == "+":
+        sys.exit(1)
+    if args[1] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif argv[2] == "-":
+    elif args[1] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == "*":
+    elif args[1] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == "/":
+    elif args[1] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
