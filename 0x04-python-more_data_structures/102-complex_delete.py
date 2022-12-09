939698ac25801125a888b7e953e28bac138957fc
Vicsituma <vicsituma45@gmail.com>
2022-12-09 08:36:54 -0800

    search_replace

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary:
+    key_list = list(a_dictionary.keys())
+    for key in key_list:
         if a_dictionary[key] == value:
             del(a_dictionary[key])
     return a_dictionary
