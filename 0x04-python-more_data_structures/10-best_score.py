ddbbd6eb7243ed41ad2a832bf129b60a8b292174
Vicsituma <vicsituma45@gmail.com>
2022-12-09 09:41:24 -0800

    correction on more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if len(a_dictionary) == 0 or a_dictionary is None:
         return None
-    w = (a_dictionary.values())
-    x = (a_dictionary.keys())
+    w = list(a_dictionary.values())
+    x = list(a_dictionary.keys())
     return x[w.index(max(w))]
