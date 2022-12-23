34b6364344df502988b77eb46457a46a6fa855d7
Vicsituma <vicsituma45@gmail.com>
2022-12-22 19:18:41 -0800

    setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""defining a square"""
+
+
+class Square:
+    """a class that defines a square"""
+    def __init__(self, size=0):
+        """initialization of the square class using size"""
+        self.__size = size
+        @property
+        def size(self):
+            """size getter"""
+            return self.__size
+        @size.setter
+        def size(self, val):
+            """sets new size values"""
+            if isinstance(size, int):
+                if size >= 0:
+                    self.__size = size
+                else:
+                    raise ValueError("size must be >= 0")
+            else:
+                raise TypeError("size must be an integer")
+
+    def area(self):
+        """calculates the area of the square"""
+        return (self.__size**2)
