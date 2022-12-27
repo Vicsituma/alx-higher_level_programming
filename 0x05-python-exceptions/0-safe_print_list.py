69fa346768ce848d67872e7e9a9a206951fc9cb3
Vicsituma <vicsituma45@gmail.com>
2022-12-27 03:07:46 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(mylist[i], end="")
-    except:
-        print("can't print")
-    return i
+    sum = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            sum += 1
+        except IndexError:
+            break
+    print()
+    return sum
